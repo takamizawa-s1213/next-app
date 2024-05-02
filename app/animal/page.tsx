0d25dc7afd688ced40ccb3809850a6db0f4e0e19
takamizawa-s1213 <aimhigh.1213@gmail.com>
2024-05-02 17:29:06 +0900

    first commit

---
+++ app/animal/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+export default function Animal() {
+    return (
+        <main className={styles.main}>
+            <p>app/animal/page.tsxです。</p>
+        </main>
+    )
+}
